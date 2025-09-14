ccc7e07b4e278f161316da8dac2e0f1591ff9224
Jamer Delgado <133704939+Jd2698@users.noreply.github.com>
2025-09-14 11:45:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Ejemplo de un API RESTFUL con autenticación usando SANCTUM
+
+# Instalación
+
+```bash
+composer install
+```
+```bash
+php artisan migrate
+```
+```bash
+php artisan serve
+```
+
+# Endpoints
+- api/auth/login
+- api/auth/register
+- api/auth/logout
+- api/user
+- api/posts
+- api/posts/{id}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        $post = $request->user()->posts()->create($request->all());
+
+        return $post;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        Gate::authorize('modify', $post);
+
+        $post->update($request->all());
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        Gate::authorize('modify', $post);
+
+        $post->delete();
+
+        return ["The post was removing succesfully"];
+    }
+}
